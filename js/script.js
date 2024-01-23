fa0e001b8936c47847d31c45badbeedbfd26c6cf
Al_P <al.e.poliakow@gmail.com>
2024-01-23 12:27:28 +1100

    JS reveal a random image

--- js/script.js
+++ js/script.js
@@ -3,10 +3,33 @@ const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
-const getImage = async function(){
+const getImage = async function () {
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await res.json();
     console.log(images);
+    selectRandomImage(images);//call the selectRandomImage() function and pass it images as a function
 };
 
-getImage();
+const selectRandomImage = function (images) {
+    //console.log(randomIndex); // to check that a number between 0 and 99 is returned
+    const randomIndex = Math.floor(Math.random() * images.length);
+    //call the selectRandomImage() function and pass it images as a function
+    const randomImage = images[randomIndex]; //square brackets
+    //console.log(randomImage);
+    displayImage(randomImage);
+};
+
+const displayImage = function(randomImage){
+    const author = randomImage.author; // dot notation for property
+    const imageAddress = randomImage.download_url;
+    //modify DOM elements
+    authorSpan.innerText = author; // change innerText of authorSpan to the value of the author variable
+    img.src = imageAddress; // set the src attribute of the img variable to the value of imageAddress
+    imgDiv.classList.remove("hide"); // requires ""
+};
+
+
+button.addEventListener("click", function(){
+    getImage();
+});
+
